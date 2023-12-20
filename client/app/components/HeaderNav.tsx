5445cfd782aeca782a67a9a5905baa10c2a3ffe0
TechnoLuckkk <ld523@cornell.edu>
2023-12-20 17:42:34 -0500

    - Added authentication

--- client/app/components/HeaderNav.tsx
+++ client/app/components/HeaderNav.tsx
@@ -1,8 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function HeaderNav() {
     return (
         <div className="flex flex-none h-12 align-middle justify-between mb-10 pt-6 text-sky-500">
             <h2 className="text-2xl font-bold">Rent Easy</h2>
-            <button className="w-28 hover:text-sky-700">List New Item</button>
+            <div className="flex flex-row gap-10">
+                <button className="w-28 hover:text-sky-700">List New Item</button>
+                <UserButton afterSignOutUrl="/"/>
+            </div>
         </div>
     )
 }
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import HeaderNav from "./components/HeaderNav";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,13 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className="flex flex-col h-screen px-8 sm:px-36">
-          <HeaderNav />
-          {children}
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <div className="flex flex-col h-screen px-8 sm:px-36">
+            <HeaderNav />
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
