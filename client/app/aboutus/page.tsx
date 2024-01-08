e13a32563d249f989c8e980605e785c03105bf7f
TechnoLuckkk <ld523@cornell.edu>
2024-01-07 22:48:40 -0500

    - Fixed &apos;

--- client/app/aboutus/page.tsx
+++ client/app/aboutus/page.tsx
@@ -13,14 +13,14 @@ export default function AboutUs() {
         In this way owner can earn passive income on their idle assets, and people needing temporary access 
         to these items wont need to buy them, instead just rent it out and save money.
         Founded by a passionate group of Cornell Tech students situated on the Roosevelt Island, 
-        we're thrilled to introduce our first prototype with the main goal of fostering a community of sharing and accessibility.</p>
+        we&apos;re thrilled to introduce our first prototype with the main goal of fostering a community of sharing and accessibility.</p>
 
         <br></br>
 
         <h1 className="text-3xl"><b>How does it work?</b></h1>  
         <br></br>
         <p><b>Owner:</b> You have have an asset lying idle in your home eg. Camera, Bicycle, Projector, you can upload it on our platform for free. 
-        Click on 'List New Item' on top right of the page to register as a owner and start listing your items. You will have to provide
+        Click on &apos;List New Item&apos; on top right of the page to register as a owner and start listing your items. You will have to provide
         your WhatsApp number, Address and Email to register as a owner. If someone wishes to rent out the products that you have listed, you will
         be reached out by them, on your provided WhatsApp number.</p>
         <br></br>
@@ -31,7 +31,7 @@ export default function AboutUs() {
 
         <h1 className="text-3xl"><b>This is just a prototype</b></h1>
         <br></br>
-        <p>You might be wondering, 'oh! what a crappy looking website without any features!' - we understand it!
+        <p>You might be wondering, &apos;oh! what a crappy looking website without any features!&apos; - we understand it!
         This is just our first prototype we have built with the only goal to gauge the product-market fit, validate our idea and talk to customers.
         As of now, we would are limiting to people only on Roosevelt island, we are not a registered company, we are not providing any protection, payment
         and delivery facilitation and are not responsible for any damage/loss incurred during the rent. We are aware that all of these things are crucial to
