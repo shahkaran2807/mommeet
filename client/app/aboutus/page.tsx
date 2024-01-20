5d6b905fdeeb216082bbf06d0bbb3f1d90631316
Karan Shah <shahkaran2807@gmail.com>
2024-01-20 11:16:36 -0500

    no message

--- client/app/aboutus/page.tsx
+++ client/app/aboutus/page.tsx
@@ -24,19 +24,27 @@ export default function AboutUs() {
         Click on &apos;List New Item&apos; on top right of the page to register as a owner and start listing your items. You will have to provide
         your WhatsApp number, Address and Email to register as a owner. If someone wishes to rent out the products that you have listed, you will
         be reached out by them, on your provided WhatsApp number.</p>
+        <p>While handing over the product make sure that the renter/customer is trustworthy and we recommend asking their address and phone number. 
+        Once you have handed over your product, go to My Listings Page by clicking on the profile icon at the top right of your screen, and mark 
+        the product as rented and make those dates unavailable by clicking on the edit option.</p>
+        <p>Once the rental period is over and you get back your product, go to My Listings Page and click the Rented button to make the product 
+        available again and change the dates to available.</p>
         <br></br>
-       <p><b>Customer:</b> If you need temporary access to an asset eg. a DSLR Camera for a holiday trip, you can reach out to people in your neighborhood
-        having those items, via our platform.</p>
+       <p><b>Customer/renter:</b> If you need temporary access to an asset eg. a DSLR Camera for a holiday trip, you can reach out to people in your neighborhood
+        having those items, via our platform. Once you request for a product, you will automatically be redirected to WhatsApp with a pre written message.</p>
+        <p>When you go to pickup the product please make sure the product is functioning properly</p>
+        <p>During your rental period please make sure to handle the product carefully and in case of damage/loss/theft you will be responsible to pay the full amount
+         of the product to the owner. </p>
 
         <br></br>
 
-        <h1 className="text-3xl"><b>This is just a prototype</b></h1>
+        <h1 className="text-3xl"><b>This is our first prototype</b></h1>
         <br></br>
         <p>You might be wondering, &apos;oh! what a crappy looking website without any features!&apos; - we understand it!
         This is just our first prototype we have built with the only goal to gauge the product-market fit, validate our idea and talk to customers.
         As of now, we would are limiting to people only on Roosevelt island, we are not a registered company, we are not providing any protection, payment
         and delivery facilitation and are not responsible for any damage/loss incurred during the rent. We are aware that all of these things are crucial to
-        build upon trust and we will implement it in the future.</p>
+        build upon trust and we will soon implement it in the future.</p>
 
         <br></br>
 
